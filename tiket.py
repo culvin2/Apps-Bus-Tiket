fc2cd306cfca62e410f1e832bbac9c7c60905d6e
Calvin Lazuardi <frequently19@gmail.com>
2020-10-21 14:11:04 +0700

    first commit

---
+++ tiket.py
@@ -0,0 +1,46 @@
+
+print('''        Banten Bus
+1. Surabaya 
+Jam keberangkatan 
+ (a)   06.00           Rp. 89.000,-
+ (b)   12.00           Rp. 100.000,-
+ (c)   15.00           Rp. 120.000,-
+
+2. Malang 
+Jam keberangkatan
+ (a)   06.00/ 07.00    Rp. 85.000,-
+ (b)   15.00           Rp. 100.000,-
+ (c)   20.00           Rp. 75.000,-
+
+3. Bali
+Jam keberangkatan
+ (a)   07.00           Rp. 155.000,-''')
+
+a = int(input("Tujuan Keberangkatan    : "))
+    if a ==1:
+        print("Selasa, Rabu, Kamis diskon 15% ")
+        print("Anak usia (0-2 tahun) dikenakan tambahan Rp. 75.000,-")
+        print("Anak usia di atas 2 tahun maka tiket dikenakan seperti orang dewasa")
+        print("Tiket belum termasuk biaya admin, iuran JASA RAHARJA, Asuransi, dan Pajak 10% dari harga tiket")
+        b = str(input("Masukkan Hari Berangkat        : "))
+        c = int(input("Masukkan Jam berangkat (a/b/c) : "))
+        d = int(input("Masukkan berapa tiket          : "))
+        print(" * Terima Kasih Telah Menggunakan Banten Bus *")
+    elif a ==2:
+        print("Selasa, Rabu, Kamis diskon 15% ")
+        print("Anak usia (0-2 tahun) dikenakan tambahan Rp. 75.000,-")
+        print("Anak usia di atas 2 tahun maka tiket dikenakan seperti orang dewasa")
+        print("Tiket belum termasuk biaya admin, iuran JASA RAHARJA, Asuransi, dan Pajak 10% dari harga tiket")
+        b = str(input("Masukkan Hari Berangkat        : "))
+        c = int(input("Masukkan Jam berangkat (a/b/c) : "))
+        d = int(input("Masukkan berapa tiket          : "))
+        print(" * Terima Kasih Telah Menggunakan Banten Bus *")
+    else:
+        print("Selasa, Rabu, Kamis diskon 15% ")
+        print("Anak usia (0-2 tahun) dikenakan tambahan Rp. 75.000,-")
+        print("Anak usia di atas 2 tahun maka tiket dikenakan seperti orang dewasa")
+        print("Tiket belum termasuk biaya admin, iuran JASA RAHARJA, Asuransi, dan Pajak 10% dari harga tiket")
+        b = str(input("Masukkan Hari Berangkat        : "))
+        c = int(input("Masukkan Jam berangkat (a/b/c) : "))
+        d = int(input("Masukkan berapa tiket          : "))
+        print(" * Terima Kasih Telah Menggunakan Banten Bus *")
